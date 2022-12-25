9aba4c488da30e7666c6989f6c959a1a47c2f0b6
Lyudmila Galkina <lygalkina@ozon.ru>
2022-12-25 22:40:58 +0300

    fix

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/sort/SortTypeUI.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/sort/SortTypeUI.java
@@ -25,7 +25,7 @@ public class SortTypeUI implements ListDiffInterface<SortTypeUI> {
 
     @Override
     public boolean theSameAs(SortTypeUI obj) {
-        return this.equals(obj);
+        return this.getSortType() == obj.getSortType();
     }
 
     @Override
